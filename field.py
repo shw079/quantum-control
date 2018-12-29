b6659e84b9360a8cb398f416505ac4d1542ab098
Yi-Che Chang <y.chang@princeton.edu>
2018-12-29 23:26:46 +0800

    Redesigned class Field to hold field data at only a single time point.

--- field.py
+++ field.py
@@ -3,18 +3,34 @@
 import numpy as np
 
 class Field(object):
-    """ A Field is a 2xn array where n is the number of time points
-    Class Field provides a container for calculated fields and functions to access them
+    """Class Field provides a container for x and y field data.
+
+    A Field instance contains the value of x- and y-component of field 
+    data at a single time point.
+
+    Parameter:
+
+        value: an nd.array object of shape (2,), (1,2), or (2,1). The 
+        first and second values correspond to the x- and y-component 
+        of the field data, respectively.
+
     """
 
-    def __init__(self, n):
-        """ Initializes a Field instance with appropriate length (n number of time points) """
-        self.value = np.zeros((n,2)) # value is one time point of field
+    def __init__(self, value):
+        """Initializes a Field instance with value set to input value."""
+        if not isinstance(value, np.ndarray):
+            errmsg = "Input value must be an np.array."
+            raise TypeError(errmsg)
+        elif value.size is not 2:
+            errmsg = "Input value must contain two scalars only."
+            raise ValueError(errmsg)
+        else:
+            self.value = value.reshape(2) # value is one time point of field
 
-    def set_value(self, i, field):
-        """ Allow for user to set values of field """
-        self.value[i,:] = field 
+    # def set_value(self, i, field):
+    #     """ Allow for user to set values of field """
+    #     self.value[i,:] = field 
 
-    def get_value(self):
-        """ Allow for user to return values of field """
-        return self.value
+    # def get_value(self):
+    #     """ Allow for user to return values of field """
+    #     return self.value
--- testField.py
+++ testField.py
@@ -9,24 +9,38 @@ from field import Field
 
 class test_Field(unittest.TestCase):
 
-    def test_input(self):
-        """check constructor"""
-        n=10
-        #t = 0
-        #field_input = np.arange(0,2)
-        field_obj = Field(n)
-        field = field_obj.get_value()
-        self.assertEqual(field.shape,(n,2))
+    # def test_input(self):
+    #     """check constructor"""
+    #     n=10
+    #     #t = 0
+    #     #field_input = np.arange(0,2)
+    #     field_obj = Field(n)
+    #     field = field_obj.get_value()
+    #     self.assertEqual(field.shape,(n,2))
 
-    def test_set_value(self):
-        """check set value"""
-        n=10
-        t = 0
-        field_input = np.arange(0,2)
-        field_obj = Field(n)
-        field_obj.set_value(t,field_input)
-        check_field = field_obj.get_value()
-        np.testing.assert_array_equal(check_field[t,:],field_input)
+    # def test_set_value(self):
+    #     """check set value"""
+    #     n=10
+    #     t = 0
+    #     field_input = np.arange(0,2)
+    #     field_obj = Field(n)
+    #     field_obj.set_value(t,field_input)
+    #     check_field = field_obj.get_value()
+    #     np.testing.assert_array_equal(check_field[t,:],field_input)
+
+    def test_init(self):
+        """Test instantiation of a Field instance."""
+        value = np.array([1,2])
+        values = [value, value.reshape(1,2), value.reshape(2,1)]
+        for v in values:
+            field = Field(v)
+            self.assertEqual( field.value.all(), v.all() )
+
+    def test_init_input(self):
+        """Test input type and shape."""
+        values = [[1,2], np.array([1, 2, 3])]
+        self.assertRaises(TypeError, Field, values[0])
+        self.assertRaises(ValueError, Field, values[1])
 
 
 if __name__ == '__main__':
