80646f6e9eb27485c15b930487d006b9bae345ed
Yi-Che Chang <y.chang@princeton.edu>
2019-01-06 16:41:07 -0500

    Corrected mu in constants.py

--- solver/constants.py
+++ solver/constants.py
@@ -9,7 +9,7 @@ m = 8
 ## Rotational rotational constant with unit converted to atomic units
 B = 4.033e-24 / 4.36e-18 
 ## Dipole moment with unit converted to atomic units
-mu = 2.3649e-30 / 8.48e-30
+mu = 2.36496e-30 / 8.48e-30
 ## Reduced Planck's constant
 hbar = 1.0
 ## 4*pi*epsilon0.
