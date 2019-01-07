151e88bda5e8e20e37042f5be2edf18d305e618e
Yi-Che Chang <y.chang@princeton.edu>
2019-01-07 10:45:24 -0500

    Updated main.py to include visualization

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ from modules.visualization import Visualization
 # Get user-specified path
 user_path = importPath.import_path()
 user_path.mainloop()
+path_in = user_path.get_coordinates()
+
+# Determine delta t and modify path array if necessary
+# Below two lines just pretend as we got the desired `dt` and 
+# `path_desired`.
+dt = 1000 #This is just for demo
+path_desired = path_in #This is just for demo
 
 # Instantiate a DataContainer object with path specified by the user
-data = DataContainer(user_path.get_coordinates)
-
-# Determine delta t
+data = DataContainer(path_desired, dt)
 
 # Solve for the required control fields
-field_solver = solvers.PathToField(data.path_desired, data.dt)
-field_solver.solve()
-data.path_actual, data.state = field_solver.export()[1:]
+s = solvers.PathToField(data.path_desired, data.dt)
+s.solve()
+data.t, data.field, data.path_actual, data.state = s.export()
 
 # Analyze resulting paths from noisy control fields
 
+
 # Visualize results
+vis = Visualization(data)
+vis.density()
+vis.trajectory()
+vis.field()
