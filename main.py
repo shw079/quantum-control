f85d0110edfd517637afd62dbc223908c305abc1
Yi-Che Chang <y.chang@princeton.edu>
2019-01-06 23:56:55 -0500

    In main.py attempted to integrate modules but yet to be done

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 ''' main.py
 '''
 
-user_path = importPath()
+from modules import importPath, solvers
+from modules.dataContainer import DataContainer
+from modules.visualization import Visualization
+
+# Get user-specified path
+user_path = importPath.import_path()
 user_path.mainloop()
-data = DataStore(user_path.get_coordinates)
 
-#calc field
-solver = PathToField( data.get_path() )
-solver.calc_field()
-data.set_field( solver.get_field() )
+# Instantiate a DataContainer object with path specified by the user
+data = DataContainer(user_path.get_coordinates)
 
-#noise analysis
- 
-v = vis(data)
-v.plot()
+# Determine delta t
+
+# Solve for the required control fields
+field_solver = solvers.PathToField(data.path_desired, data.dt)
+field_solver.solve()
+data.path_actual, data.state = field_solver.export()[1:]
+
+# Analyze resulting paths from noisy control fields
+
+# Visualize results
