8e470151218aa5b98303cb0e46c721410fe41175
Shuwen Yue <syue@princeton.edu>
2019-01-08 18:52:46 -0500

    removed import transform in main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '.', 'modules'))
 import importPath, solvers
-from transform import transform_path
 from dataContainer import DataContainer
 from visualization import Visualization
 
