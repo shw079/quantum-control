fdee63be09d65e579314c1d6aa492eec4806192f
Shuwen Yue <syue@princeton.edu>
2019-01-08 09:49:59 -0500

    fixed bugs in transform.py

--- modules/transform.py
+++ modules/transform.py
@@ -7,31 +7,37 @@ import constants as const
 def transform_path(path):
     
     TOL = 0.5;
-    if (np.amax(math.abs(path[:,0]))>TOL or np.amax(math.abs(path[:,1]))>TOL):
-        path[:,0] = path[:,0]*(TOL/np.amax(math.abs(path[:,0])))
-        path[:,1] = path[:,1]*(TOL/np.amax(math.abs(path[:,1])))
+    # this if loop may need more testing
+    if (np.amax(abs(path[:,0]))>TOL or np.amax(abs(path[:,1]))>TOL):
+        path[:,0] = path[:,0]*(TOL/np.amax(abs(path[:,0])))
+        path[:,1] = path[:,1]*(TOL/np.amax(abs(path[:,1])))
 
     lengthxy=0
     for k in range(1,len(path)):
         lengthxy = math.sqrt((path[k,0]-path[k-1,0])**2+(path[k,1]-path[k-1,1])**2)+lengthxy
-
+ 
     dt = 1000
     Trot = 2*math.pi*const.hbar/const.B
     max_t = 10*lengthxy*Trot/(2*math.pi)
-    t = np.linspace(0, max_t ,max_t/dt+1)
+    t = np.arange(0,max_t,dt)
+    max_t = t[-1]
 
     t_sigmoid = np.linspace(-5,0,len(t))
-    sigmoid = np.exp(t_sigmoid) @ (1/(np.exp(t_sigmoid)[:]+1))
+
+    sigmoid = np.exp(t_sigmoid)[:]/(np.exp(t_sigmoid)[:]+1)
     sigmoid = sigmoid*(1/(sigmoid[-1]-sigmoid[0]))
     sigmoid = t[-1]*(sigmoid[:]+(1-sigmoid[-1]));
 
-    t_raw = np.linspace(0,t[-1],t[-1]*(1/(len(path))))
+    t_raw = np.linspace(0,1,len(path))
+    t_raw = t_raw*max_t
+
     Ox = np.interp(sigmoid, t_raw, path[:,0])
     Oy = np.interp(sigmoid, t_raw, path[:,1])
 
-    Ox = savitzky_golay(Ox, 4*math.floor(len(Ox)/len(Oxraw))+1, 5)
-    Oy = savitzky_golay(Oy, 4*math.floor(len(Ox)/len(Oxraw))+1, 5)
+    print(8*math.floor(len(Ox)/len(path[:,0]))+1)
+    Ox = savitzky_golay(Ox, 8*math.floor(len(Ox)/len(path[:,0]))+1, 5)
+    Oy = savitzky_golay(Oy, 8*math.floor(len(Ox)/len(path[:,0]))+1, 5)
 
-    new_path = np.stack((Ox Oy))
+    new_path = np.stack((Ox,Oy))
 
     return new_path, dt
