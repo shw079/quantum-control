02a5d6407733a5b04a6b0c17ee65cc78f16a235a
Shuwen Yue <syue@princeton.edu>
2019-01-08 15:26:52 -0500

    editted main

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ user_path = importPath.import_path()
 user_path.mainloop()
 path_in = user_path.get_coordinates()
 
-#filename = "tests/example_user_data.dat"
-#root = importPath.import_path()
-#root.load_from_file(filename)
-#path_in = root.get_coordinates()
-
 # Determine delta t and modify path array if necessary
 path_desired, dt = transform_path(path_in)
 
@@ -29,15 +24,10 @@ s.solve()
 data.t, data.field, data.path_actual, data.state = s.export()
 
 
-#plt.plot(data.path_desired[:,0],data.path_desired[:,1],label='desired')
-#plt.plot(data.path_actual[:,0],data.path_actual[:,1],label='actual')
-#plt.legend()
-#plt.show()
-
 # Analyze resulting paths from noisy control fields
 
 # Visualize results
 vis = Visualization(data)
 vis.density()
 vis.trajectory()
-#vis.field()
+vis.fields()
