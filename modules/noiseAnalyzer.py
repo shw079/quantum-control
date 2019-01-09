31247daade274656661e5a5d473a906a86847d0e
Zahra Bajalan <zbajalan@princeton.edu>
2019-01-08 20:26:49 -0500

    now the code is written, it passes 3 tests

--- modules/noiseAnalyzer.py
+++ modules/noiseAnalyzer.py
@@ -8,35 +8,65 @@ import functions as f
 import constants as const
 
 class NoiseAnalyser(object):
+    """this part does some noise analysis for a given field
 
-  """this part does some noise analysis for a given field
+    """
 
-  """
-  def __init__(self,smoothfield,dt,variance,numfield):
-      self.field=smoothfield
-      self.dt=dt
-      self.numfield=numfield
-      self.variance=variance
+    def __init__(self,smoothfield,dt,variance,numfield):
+        self.field=smoothfield
+        self.dt=dt
+        self.numfield=numfield
+        self.variance=variance
+        self.path=np.empty((len(self.field), 2 * self.numfield),dtype=complex)
+        
  
-  def calc_noisy_field(self):
-      for x in range(numfield):
-          sx=np.random.normal(0, self.variance, len(self.field))
-          sy=np.random.normal(0, self.variance, len(self.field))
-          noisy_field[:,0]=self.field[:,0]+sx.transpose
-          noisy_field[:,1]=self.field[:,1]+sx.transpose
-    
+    def calc_noisy_field(self):
+        noisy_field=np.empty((len(self.field), 2 * self.numfield), dtype=complex)
+        for i in range(self.numfield):
+            sx=np.random.normal(0, self.variance, len(self.field))
+            sy=np.random.normal(0, self.variance, len(self.field))
+            noisy_field[:,0+i*2]=self.field[:,0]+np.transpose(sx)
+            noisy_field[:,1+i*2]=self.field[:,1]+np.transpose(sy)
+            
+        self.noisy_field = noisy_field.astype(float)
+       
      
-  def calc_path(self):
-      path_solver = FieldToPath(noisy_field, dt)
-      
-      # Then invoke the solve() method of the path_solver object
-      path_solver.solve()
-      
-      # Finally, use the export() method to export time, path, states (in this
-      # order.) But one can also just export 'path' by the following line.
-      path = path_solver.export()
+    def calc_path(self):
+        self.path=np.empty((len(self.field), 2 * self.numfield),dtype=complex)
+        for i in range(self.numfield):
+            path_solver = FieldToPath(self.noisy_field[:,[i*2,i*2+1]], dt)
+            # Then invoke the solve() method of the path_solver object
+            path_solver.solve()
+            self.path[:,[i*2,i*2+1]]= path_solver.export()[1]
+       
+        
+ 
+    def calc_statistic(self):
+        xcollec= np.zeros((len(self.path),self.numfield))
+        ycollec= np.zeros((len(self.path),self.numfield))
+        for j in range(len(self.path)):
+            for i in range(2*self.numfield):
+                mod=i%2
+                if mod==0:
+                    xcollec[j,int(i/2)]=self.path[j,i]
+                else:
+                    ycollec[j,int((i-1)/2)]=self.path[j,i]
+        pathxmean = np.zeros(len(self.path))
+        pathymean = np.zeros_like(pathxmean)
+        pathxvar = np.zeros_like(pathxmean)
+        pathyvar = np.zeros_like(pathxmean)
+        for i in range(len(self.path)):
+            pathxmean[i]=sum(xcollec[i,:])/self.numfield
+            pathymean[i]=sum(ycollec[i,:])/self.numfield
+            pathxvar[i] =np.var(xcollec[i,:])
+            pathyvar[i] =np.var(ycollec[i,:])
+        self.pathmean = np.stack((pathxmean,pathymean),axis=1)
+        self.pathvar = np.stack((pathxvar,pathyvar),axis=1)
 
-  def calc_statistic(self):
-      pass
+    def analyze(self):
+        self.calc_noisy_field()
+        self.calc_path()
+        self.calc_statistic()
 
+        return self.pathmean.astype(float), self.pathvar.astype(float)
 
--- tests/test_noiseAnalyzer.py
+++ tests/test_noiseAnalyzer.py
@@ -17,11 +17,25 @@ class test_noiseAnalyzer(unittest.TestCase):
         self.assertTrue(myNA.dt == dt)
 
     def test_2(self):
-        pass
-
+        input_field = np.arange(10).reshape((5,2))
+        dt = 1000
+        myNA = NoiseAnalyser(input_field, dt, 0.0000001, 1)
+        myNA.calc_noisy_field()
+        np.testing.assert_array_almost_equal(myNA.noisy_field,input_field) 
+    
+    def test_3(self):
+        input_path=np.arange(10).reshape((5,2))
+        input_field = np.arange(10).reshape((5,2))
+        dt = 1000
+        myNA = NoiseAnalyser(input_field, dt, 1, 1)
+        myNA.path=input_path
+        myNA.calc_statistic()
+        np.testing.assert_array_equal( myNA.pathmean,input_path)
+        np.testing.assert_array_equal( myNA.pathvar,np.zeros((5,2)))
+        
 if __name__ == '__main__':
     unittest.main()
-
+ 
 #self.assertTrue()
 #self.assertEqual(arg1 ,arg2 )
 #self.assertAlmostEqual(number1,number2)
