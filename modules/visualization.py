9bd42adbc72fc00a43f8bcdb22599015cac692bb
Shuo Wang <shuow@princeton.edu>
2019-01-12 21:39:39 -0500

    Added numpy style docstring

--- modules/visualization.py
+++ modules/visualization.py
@@ -1,13 +1,3 @@
-"""!@namespace
-The visualization module uses data in a DataStore object for plotting.
-This module allows plotting of:
-    1. control fields amplitude over time for both x and y fields.
-    2. actual and expected trajectory of the rigid rotor for dimensions
-       x and y over time, together with a phase plot of x and y tracks.
-    3. a heatmap of probability density for rotational angle over time.
-"""
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,50 +7,90 @@ import constants
 class Visualization:
     """Class for visualizing data of a DataStore object.
 
+    The visualization module uses data in a DataContainer object for 
+    plotting. This module allows plotting of:
+
+    - control fields amplitude over time for both x and y fields.
+    - actual and expected trajectory of the rigid rotor for dimensions
+      x and y over time, together with a phase plot of x and y 
+      trajectories.
+    - a heatmap of probability density for rotational angle over time.
+
+    Parameters
+    ----------
+    dat : object
+        A DataContainer used for plotting.
+
+
+    Attributes
+    ----------
+    t : numpy.array, shape=(n,).
+        Time points.
+
+    m : int
+        Magnetic quantum number.
+
+    state : numpy.array, shape=((2m+1, n))
+        System state at each time point.
+
+    field : numpy.array, shape=(n, 2)
+        Control fields.
+
+    cos_phi_actual : numpy.array, shape=(n,)
+        Actual x trajectory.
+
+    sin_phi_actual : numpy.array, shape=(n,)
+        Actual y trajectory.
+
+    cos_phi_desired : numpy.array, shape=(n,)
+        Desired x trajectory.
+
+    sin_phi_desired : numpy.array, shape=(n,)
+        Desired y trajectory.
+
+    mean_cos_phi_actual_noise : numpy.array, shape=(n,)
+        Mean actual x trajectory with noise.
+
+    mean_sin_phi_actual_noise : numpy.array, shape=(n,)
+        Mean actual y trajectory with noise.
+
+    noise_stat_var : numpy.array, shape=(n, 2)
+        Variance of actual trajectorys with noise.
+
     """
     def __init__(self, dat):
-        """!
-        Construct a Visualization object.
-        
-        @param dat the DataContainer object used for plotting.
-        """
-        ## time points
         self.t = dat.t
-        ## system state at each time point
         self.state = dat.state
-        ## magnetic quantum number
         self.m = constants.m
-        ## control fields
         self.field = dat.field
-        ## actual x track
         self.cos_phi_actual = dat.path_actual[:, 0]
-        ## actual y track 
         self.sin_phi_actual = dat.path_actual[:, 1]
-        ## desired x track
         self.cos_phi_desired = dat.path_desired[:, 0]
-        ## desired y track
         self.sin_phi_desired = dat.path_desired[:, 1]
-        # mean actual x track w/ noise
         self.mean_cos_phi_actual_noise = dat.noise_stat_mean[:, 0]
-        # mean actual y track w/ noise
         self.mean_sin_phi_actual_noise = dat.noise_stat_mean[:, 1]
-        # variance of actual tracks w/ noise
         self.noise_stat_var = dat.noise_stat_var
 
     def density(self, n_grid=100, out=None):
-        """!
-        Plot a probability density heatmap over time for different 
+        """Plot a probability density heatmap over time for different 
         rotational angles. 
 
-        @param n_grid number of equally spaced rotational angles to 
-                      calculate probability density between 0 and 
-                      2&pi;. Default to 100.
+        Parameters
+        ----------
+        n_grid : int, optional (default=100)
+            Number of equally spaced rotational angles to calculate 
+            probability density between 0 and 2:math:`pi`. Default 
+            to 100.
 
-        @param out the path to save the figure if not None. If None,
-                   show the figure in the console. Default to None.
+        out : str, optional (default=None)
+            The path to save the figure if not None. If None, show 
+            the figure in the console. Default to None.
 
-        @return a numpy ndarray for probability density for plotting, 
-                with shape of (n_grid, self.t).
+        Returns
+        -------
+        numpy.array, shape=(n_grid, n)
+            A numpy ndarray for probability density for plotting.
+
         """
         # calculate probability density
         # get equally spaced points in [0, 2 * pi)
@@ -106,53 +136,61 @@ class Visualization:
         return proba
     
     def trajectory(self, noise=True, out=None):
-        """!@brief Plot trajectories of the rigid rotor.
+        """Plot trajectories of the rigid rotor.
 
-        Plot actual and expected trajectories of x (i.e. <cos(&phi;)>) 
-        and y (i.e. <sin(&phi;)>) over time and also the phase plot of
-        x and y.
+        Plot actual and expected trajectories of x (i.e. 
+        <cos(:math:`phi`)>) and y (i.e. <sin(:math:`phi`)>) over time 
+        and also the phase plot of x and y.
 
-        @param out the path to save the figure if not None. If None,
-                   show the figure in the console. Default to None.
+        Parameters
+        ----------
+        noise : bool, optional (default=True)
+            Whether to plot mean trajectories under noise over time.
+            Default to True.
+
+        out : str
+            The path to save the figure if not None. If None, show 
+            the figure in the console. Default to None.
+
         """
         # set up grids
         fig = plt.figure(figsize=(20.5, 10))
         grid = plt.GridSpec(2, 4, wspace=0.5)
 
         # trajectories over time
-        # x track
+        # x trajectory
         ax1 = fig.add_subplot(grid[0, :2])
 
         ax1.plot(self.t, self.cos_phi_actual, color="blue", lw=2,
-                 alpha=0.6, label="actual x track" if not noise else \
-                                  "actual x track, w/o noise")
+                 alpha=0.6, label="actual x trajectory" if not noise else \
+                                  "actual x trajectory, w/o noise")
 
         if noise:
             ax1.plot(self.t, self.mean_cos_phi_actual_noise,
                      color="blue", lw=2, alpha=0.6, ls=":",
-                     label="mean actual x track, w/ noise")
+                     label="mean actual x trajectory, w/ noise")
 
         ax1.plot(self.t, self.cos_phi_desired, color="blue", lw=2,
-                 ls="-.", label="expected x track")
+                 ls="-.", label="expected x trajectory")
 
         ax1.set_ylabel(u"<cos(${\phi}$)>", fontsize=14)
         ax1.set_ylim(-1,1)
         ax1.legend(loc="upper left", fontsize=12)
 
-        # y track
+        # y trajectory
         ax2 = fig.add_subplot(grid[1, :2])
 
         ax2.plot(self.t, self.sin_phi_actual, color="red", lw=2,
-                 alpha=0.6, label="actual y track" if not noise else \
-                                  "actual y track, w/o noise")
+                 alpha=0.6, label="actual y trajectory" if not noise else \
+                                  "actual y trajectory, w/o noise")
  
         if noise:
             ax2.plot(self.t, self.mean_sin_phi_actual_noise,
                      color="red", lw=2, alpha=0.6, ls=":",
-                     label="mean actual y track, w/ noise")
+                     label="mean actual y trajectory, w/ noise")
 
         ax2.plot(self.t, self.sin_phi_desired, color="red", lw=2,
-                 ls="-.", label="expected y track")
+                 ls="-.", label="expected y trajectory")
 
         ax2.set_ylabel(u"<sin(${\phi}$)>", fontsize=14)
         ax2.set_ylim(-1,1)
@@ -193,12 +231,15 @@ class Visualization:
             plt.show()
 
     def fields(self, out=None):
-        """!
-        Plot contol fields amplitude over time for both x and y 
+        """Plot contol fields amplitude over time for both x and y 
         dimensions.
 
-        @param out the path to save the figure if not None. If None,
-                   show the figure in the console. Default to None.
+        Parameters
+        ----------
+        out : str
+            The path to save the figure if not None. If None, show 
+            the figure in the console. Default to None.
+
         """
         plt.figure(figsize=(10, 10))
 
@@ -220,20 +261,26 @@ class Visualization:
         else:
             plt.show()
 
-    def noise_variance(self, out=None)
-        """Plot variance of x and y trajectories over time under noise.
+    def noise_variance(self, out=None):
+        """Plot variance of trajectories over time under noise.
+
+        Parameters
+        ----------
+        out : str
+            The path to save the figure if not None. If None, show
+            the figure in the console. Default to None.
 
         """
         plt.figure(figsize=(10, 10))
 
         plt.plot(self.t, self.noise_stat_var[:, 0], color="blue",
-                 lw=2, label="u"<cos(${\phi}$)>"")
+                 lw=2, label=u"<cos(${\phi}$)>")
         plt.plot(self.t, self.noise_stat_var[:, 1], color="red",
-                 lw=2, label="u"<sin(${\phi}$)>"")
+                 lw=2, label=u"<sin(${\phi}$)>")
 
         plt.xlabel("Time [ps]", fontsize=14)
         plt.ylabel("Variance", fontsize=14)
-        plt.title("Variance of trajectory over time under noise", 
+        plt.title("Variance of trajectory under noise", 
                   fontsize=20)
         plt.legend(loc="upper right", fontsize=12)
 
